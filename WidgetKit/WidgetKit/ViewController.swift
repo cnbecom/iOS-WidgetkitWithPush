fc55e88bf58c80f3c6834eb9e475db84ad482b78
Chris Becom <chris.becom@epicgames.com>
2020-07-31 12:44:03 -0400

    Added inital project files. Updated gitignore

---
+++ WidgetKit/WidgetKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetKit
+//
+//  Created by Chris Becom on 7/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
